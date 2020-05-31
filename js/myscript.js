333f7c2aa9f2b85431721010a27b904398a87e46
Md MirajAnsari <mirajhadish48@gmail.com>
2020-05-31 16:51:15 +0530

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,41 @@
+const URL = "https://covid19.mathdro.id/api";
+
+
+let app = angular.module('MyApp',[]);
+
+app.controller("MyCtrl", ($scope, $http)=>{
+    $scope.title = "Stay Home Stay safe";
+
+        console.log("APP LOADED");
+        
+        $http.get(URL).then( (response)=>{
+
+            // console.log("Inside success");
+            console.log(response.data);
+
+            $scope.all_data=response.data;
+        }, 
+        (error)=>{
+            console.log(error);
+        } 
+        );
+//  get country data
+        $scope.get_c_data = () => {
+            let country = $scope.c;
+            if(country == "") {
+                $scope.c_data=undefined;
+                return;
+            }
+
+            $http.get(`${URL}/countries/${country}`)
+            .then((response) => {
+                console.log(response.data)
+                $scope.c_data = response.data;
+            }, 
+            (error) => {
+                console.log(error);
+            }
+            );
+        };
+
+});
